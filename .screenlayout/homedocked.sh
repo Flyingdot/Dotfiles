5000a81771fb773519fe68a06b40f4babf4d0f24
Dani Meier <dani.meier@gmail.com>
2020-06-04 01:30:24 +0200

    Add more screen-layouts
    Moved dpi-settings to xrandr-screenlayouts instead of Xresources. Not
    sure if this is the best solution...

---
+++ .screenlayout/homedocked.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+xrandr --dpi 108 \
+	--output DP-1 --mode 5120x1440 \
+	--output DP-2 --scale .5x.5
---
+++ .screenlayout/laptop.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --dpi 192 \
+	--output DP-2 --scale 1x1 --mode 3840x2160 --fb 3840x2160+0+0
