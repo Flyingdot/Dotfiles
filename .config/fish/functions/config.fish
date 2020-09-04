2dfee4688ecc634d225ae891b9eee71421b278a9
Dani Meier <dani.meier@gmail.com>
2020-09-04 22:36:49 +0200

    Updated fish-configuration
     - added alias for vim=nvim

--- .config/fish/functions/config.fish
+++ .config/fish/functions/config.fish
@@ -2,3 +2,4 @@
 function config --wraps='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME' --wraps='/usr/bin/git --git-dir=/home/dani/.cfg/ --work-tree=/home/dani' --description 'alias config /usr/bin/git --git-dir=/home/dani/.cfg/ --work-tree=/home/dani'
   /usr/bin/git --git-dir=/home/dani/.cfg/ --work-tree=/home/dani $argv;
 end
+
