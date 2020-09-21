6093d4385f02520037a630b1ce5958844b168314
Dani Meier <dani.meier@gmail.com>
2020-09-21 17:36:49 +0200

    Configured polybar for multimonitor-support

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,7 +3,7 @@
 killall -q polybar
 
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar example >>/tmp/polybar1.log 2>&1 & disown
-#polybar bar2 >>/tmp/polybar2.log 2>&1 & disown
+polybar dp0 >>/tmp/polybar1.log 2>&1 & disown
+polybar dp1 >>/tmp/polybar2.log 2>&1 & disown
 
 echo "Bars launched..."
