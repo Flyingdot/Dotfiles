56deb44c596245cfc367fa6f6f3a7445f69419f1
Dani Meier <dani.meier@gmail.com>
2021-01-28 09:47:58 +0100

    Fixed polybar-issues with 49docked2 profile
     - show same polybar on each monitor, not nice but at least predictable
     - left-right switch monitors, because it just makes more sense like
       this

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
-
 killall -q polybar
 
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar main >>/tmp/polybar1.log 2>&1 & disown
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload main &
+fi
+
+
+# echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+# polybar main >>/tmp/polybar1.log 2>&1 & disown
 
 echo "Bars launched..."
