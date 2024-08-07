26214c615ff9e94d32313c26d42e4bb5cb4914d5
Dani Meier <me@danimeier.dev>
2024-08-07 20:48:34 +0200

    doc(nvim): document some keymaps

--- .config/nvim/lua/flyingdot/plugins/harpoon.lua
+++ .config/nvim/lua/flyingdot/plugins/harpoon.lua
@@ -4,20 +4,20 @@ return {
 		local mark = require("harpoon.mark")
 		local ui = require("harpoon.ui")
 
-		vim.keymap.set("n", "<leader>a", mark.add_file)
-		vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+		vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Add file to harpoon" })
+		vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Toggle harpoon quick menu" })
 
 		vim.keymap.set("n", "<leader>pa", function()
 			ui.nav_file(1)
-		end)
+		end, { desc = "Navigate harpoon files" })
 		vim.keymap.set("n", "<leader>pb", function()
 			ui.nav_file(2)
-		end)
+		end, { desc = "Navigate harpoon files" })
 		vim.keymap.set("n", "<leader>pc", function()
 			ui.nav_file(3)
-		end)
+		end, { desc = "Navigate harpoon files" })
 		vim.keymap.set("n", "<leader>pd", function()
 			ui.nav_file(4)
-		end)
+		end, { desc = "Navigate harpoon files" })
 	end,
 }
