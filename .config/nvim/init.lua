2f82f8e608e21ae7b24ab14544808a89dd04c3bf
Dani Meier <me@danimeier.dev>
2024-10-04 22:29:59 +0200

    feat(neovim): updated configuration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,25 +1,32 @@
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 local uv = vim.uv or vim.loop
 
 -- Auto-install lazy.nvim if not present
 if not uv.fs_stat(lazypath) then
-	print('Installing lazy.nvim....')
+	print("Installing lazy.nvim....")
 	vim.fn.system({
-		'git',
-		'clone',
-		'--filter=blob:none',
-		'https://github.com/folke/lazy.nvim.git',
-		'--branch=stable', -- latest stable release
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
 		lazypath,
 	})
-	print('Done.')
+	print("Done.")
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 require("flyingdot")
 
-require('lazy').setup({
-	{ import = 'flyingdot.plugins' },
+require("lazy").setup({
+	{ import = "flyingdot.plugins" },
 })
 
+vim.cmd([[
+function OpenMarkdownPreview (url)
+  execute "silent ! chrome --new-window --app=" . a:url
+endfunction
+]])
+
+vim.g.mkdp_browserfunc = "OpenMarkdownPreview"
--- .config/nvim/lua/flyingdot/plugins/lspconfig.lua
+++ .config/nvim/lua/flyingdot/plugins/lspconfig.lua
@@ -69,8 +69,8 @@ return {
 					capabilities = capabilities,
 				})
 			end,
-			["tsserver"] = function()
-				lspconfig["tsserver"].setup({
+			["ts_ls"] = function()
+				lspconfig["ts_ls"].setup({
 					init_options = {
 						plugins = {
 							{
